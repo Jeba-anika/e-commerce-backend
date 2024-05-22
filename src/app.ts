e11a4ebc42d2be11f088d5f585570f6e8f310b6c
Jeba Anika <anikanur98@gmail.com>
2024-05-22 12:01:10 +0600

    order folder structure done

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import cors from 'cors'
 import express, { Request, Response } from 'express'
+import { OrderRouter } from './app/modules/orders/orders.router'
 import { ProductRoutes } from './app/modules/products/products.router'
 
 const app = express()
@@ -10,6 +11,7 @@ app.use(cors())
 
 //application routes
 app.use('/api/products', ProductRoutes)
+app.use('/api/orders', OrderRouter)
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello world!')
---
+++ src/app/modules/orders/orders.service.ts
@@ -0,0 +1,10 @@
+import { IOrder } from './orders.interface'
+
+const createOrder = async (order: IOrder) => {}
+
+const getAllOrders = async () => {}
+
+export const OrderService = {
+  createOrder,
+  getAllOrders,
+}
