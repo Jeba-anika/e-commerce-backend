d2dfba34c4d5446b82c908704ccab239b6ec3aac
Jeba Anika <anikanur98@gmail.com>
2024-05-22 14:43:36 +0600

    linting and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,8 @@ export default [
       'no-unused-expressions': 'error',
       'prefer-const': 'error',
       'no-console': 'warn',
-      'no-undef': 'error'
+      'no-undef': 'error',
+      '@typescript-eslint/no-explicit-any': 'warn'
     }
   },
   {
